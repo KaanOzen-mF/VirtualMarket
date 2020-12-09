cc8856d2c4553d0bcd6d334976cfa9eeaf7fcfee
KaanOzen-mF <74866163+KaanOzen-mF@users.noreply.github.com>
2020-12-09 18:54:30 +0300

    some images login and home page button

--- app/src/main/java/com/kaanozen/virtualmarket/ProfilePageActivity.kt
+++ app/src/main/java/com/kaanozen/virtualmarket/ProfilePageActivity.kt
@@ -5,20 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
-
+class ProfilePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile_page)
     }
 
     fun homePageOnClick(view : View){
-
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 
+
     fun profileOnClick(view: View){
-        val intent = Intent(this,ProfilePageActivity::class.java)
-        startActivity(intent)
+
 
     }
 }
